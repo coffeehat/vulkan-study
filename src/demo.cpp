7c60b351d6b576fd22f424cc070e399a692b042c
Hanxiang.Li <hanx.leee@gmail.com>
2023-08-07 21:52:40 +0800

    Create glfw window

--- src/demo.cpp
+++ src/demo.cpp
@@ -5,26 +5,45 @@
 #include <stdexcept>
 #include <cstdlib>
 
+const uint32_t WIDTH = 800;
+const uint32_t HEIGHT = 600;
+
 class HelloTriangleApplication {
 public:
   void run() {
+    initWindow();
     initVulkan();
     mainLoop();
     cleanup();
   }
 
 private:
+  void initWindow() {
+    glfwInit();
+    // GLFW_CLIENT_API is the attribute to set, GLFW_NO_API is the value
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE); // so this is no resizable
+    window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan demo", nullptr, nullptr);
+
+  }
+
   void initVulkan() {
 
   }
 
   void mainLoop() {
-
+    while (!glfwWindowShouldClose(window)) {
+      glfwPollEvents();
+    }
   }
 
   void cleanup() {
-
+    glfwDestroyWindow(window);
+    glfwTerminate();
   }
+
+private:
+  decltype(glfwCreateWindow(0, 0, "", nullptr, nullptr)) window;
 };
 
 int main() {
