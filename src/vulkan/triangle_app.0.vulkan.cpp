7842ae968131750d5e9cf5df7c7b3ba2345d9f84
Hanxiang.Li <hanx.leee@gmail.com>
2023-08-16 09:24:57 +0800

    Add some logs

--- src/vulkan/triangle_app.0.vulkan.cpp
+++ src/vulkan/triangle_app.0.vulkan.cpp
@@ -1,16 +1,29 @@
 #include "../triangle_app.hpp"
 
+#include <iostream>
+
 void HelloTriangleApplication::initVulkan() {
+  std::cout << "Run Create Instance" << std::endl;
   createInstance();
+  std::cout << "Run Create Surface" << std::endl;
   createSurface();
+  std::cout << "Run Pick Physical Device" << std::endl;
   pickPhysicalDevice();
+  std::cout << "Run Create Logical Device" << std::endl;
   createLogicalDevice();
+  std::cout << "Run Create Swap Chain" << std::endl;
   createSwapChain();
+  std::cout << "Run Create Image Views" << std::endl;
   createImageViews();
+  std::cout << "Run Create Render Pass" << std::endl;
   createRenderPass();
+  std::cout << "Run Create Graphics Pipeline" << std::endl;
   createGraphicsPipeline();
+  std::cout << "Run Create Frame Buffers" << std::endl;
   createFramebuffers();
+  std::cout << "Run Create Command Pool" << std::endl;
   createCommandPool();
+  std::cout << "Run Create Command Buffer" << std::endl;
   createCommandBuffer();
 }
 
